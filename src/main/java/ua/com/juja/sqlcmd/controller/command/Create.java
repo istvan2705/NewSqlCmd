8f7b94f4274e1219dcf942e83f6a9f2502981e4e
Stepan <stepankish@hmail.com>
2018-05-13 21:46:31 +0300

    changes

--- src/main/java/ua/com/juja/sqlcmd/controller/command/Create.java
+++ src/main/java/ua/com/juja/sqlcmd/controller/command/Create.java
@@ -6,15 +6,16 @@ import ua.com.juja.sqlcmd.model.DatabaseManager;
 import ua.com.juja.sqlcmd.view.View;
 
 import java.sql.SQLException;
+import java.util.List;
 
-public class Create implements Command {
+public class Create extends DataClass implements Command {
     private DatabaseManager manager;
     private View view;
 
-    public Create(DatabaseManager manager, View view) {
+    public Create(DatabaseManager manager,View view ) {
         this.manager = manager;
         this.view = view;
-    }
+        }
 
     @Override
     public boolean canProcess(String command) {
@@ -24,25 +25,18 @@ public class Create implements Command {
     @Override
     public void process(String command) {
 
-        String[] data = command.split(SEPARATOR);
-        if (data.length < 4) {
+        List<String> data = getTableData(command);
+        if (data.size() < 4) {
             view.write(String.format("Error entering command '%s'. Should be 'create|tableName|column1|column2|...|columnN", command));
             return;
         }
-
-        String tableName = data[1];
-        DataSet columns = new DataSet();
-        for (int i = 2; i < data.length; i++) {
-            columns.put(data[i], i);
-        }
-
+        String tableName = getTableName(data);
+        DataSet columns = getData(data);
         try {
             manager.create(tableName, columns);
             view.write(String.format("The table '%s' has been created", tableName));
         } catch (SQLException e) {
             view.write(String.format(SQL_EXCEPTION_MESSAGE, e.getMessage()));
         }
-
-
     }
 }
