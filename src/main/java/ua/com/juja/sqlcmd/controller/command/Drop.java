37879643b3ef4ed6705588516a94a0548b7af64b
Stepan <stepankish@hmail.com>
2018-05-13 21:45:24 +0300

    changes

--- src/main/java/ua/com/juja/sqlcmd/controller/command/Drop.java
+++ src/main/java/ua/com/juja/sqlcmd/controller/command/Drop.java
@@ -5,8 +5,10 @@ import ua.com.juja.sqlcmd.model.DatabaseManager;
 import ua.com.juja.sqlcmd.view.View;
 
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
 
-public class Drop implements Command {
+public class Drop extends DataClass implements Command {
     private DatabaseManager manager;
     private View view;
 
@@ -22,13 +24,12 @@ public class Drop implements Command {
 
     @Override
     public void process(String command) {
-        String[] data = command.split(SEPARATOR);
-        if (data.length != 2) {
+        List<String> data = getTableData(command);
+        if (data.size() != 2) {
             view.write(String.format("Error entering command '%s', it should be'drop|tableName", command));
             return;
         }
-        String tableName = data[1];
-
+        String tableName = getTableName(data);
         try {
             manager.deleteTable(tableName);
             view.write(String.format("The table '%s' has been deleted", tableName));
