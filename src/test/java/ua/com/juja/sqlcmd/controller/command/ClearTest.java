c462f2cd90ff4773a75da4cccdc630815bb8c706
Stepan <stepankish@hmail.com>
2018-05-13 21:54:08 +0300

    changes

--- src/test/java/ua/com/juja/sqlcmd/controller/command/ClearTest.java
+++ src/test/java/ua/com/juja/sqlcmd/controller/command/ClearTest.java
@@ -38,22 +38,22 @@ public class ClearTest {
     @Test
     public void testClearTableIfContentExists() throws SQLException {
         String tableName = "teachers";
-       // when(!manager.clear(tableName)).thenReturn(true);
         command.process("clear|" + tableName);
         verify(manager).clear(tableName);
-        verify(view).write(String.format("The content of table '%s' has been deleted", tableName));
+        verify(view).write("You are trying to clear the contents of an empty table");
     }
 
     @Test
     public void testNotClearTableIfContentNotExists() throws SQLException {
         String tableName = "teachers";
-   //     when(manager.clear(tableName)).thenReturn(false);
-        command.process("clear|" + tableName);
-        verify(manager).clear(tableName);
-        view.write("You are trying to clear the contents of an empty table");
+        try {
+            command.process("clear|" + tableName);
+            verify(manager).clear(tableName);
+
+        } catch (SQLException e) {
+            view.write(String.format("Can not execute command  due to: %s", e.getMessage()));
+        }
     }
-
-
 }
 
 
