bb0248ab4166faad50a828a5345b778827fa487b
Stepan <stepankish@hmail.com>
2018-05-13 21:58:01 +0300

    changes

--- src/test/java/ua/com/juja/sqlcmd/controller/command/ConnectTest.java
+++ src/test/java/ua/com/juja/sqlcmd/controller/command/ConnectTest.java
@@ -9,24 +9,20 @@ import ua.com.juja.sqlcmd.view.View;
 import java.sql.SQLException;
 
 import static junit.framework.TestCase.assertTrue;
-import static junit.framework.TestCase.fail;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.mockito.Mockito.*;
 
-public class ConnectTest {
+public class ConnectTest  {
 
-    private View view;
-    private DatabaseManager manager;
-    private Command command;
-
+    public View view;
+    public DatabaseManager manager;
+    public Command command;
     @Before
     public void init() {
         manager = mock(DatabaseManager.class);
         view = mock(View.class);
         command = new Connect(manager, view);
     }
-
     @Test
     public void testConnectCanProcess() {
         assertTrue(command.canProcess("connect|Academy|postgres|1401198n"));
@@ -44,31 +40,21 @@ public class ConnectTest {
         String databaseName = "Academy";
         String userName = "postgres";
         String password = "1401198n";
-
         command.process("connect|Academy|postgres|1401198n");
         verify(manager).connect(databaseName, userName, password);
         view.write(String.format("You have login to database '%s' successfully!", databaseName));
     }
 
-
     @Test
-    public void testConnectFailed() throws SQLException{
+    public void testConnectFailed() throws SQLException {
         String databaseName = "Academy";
         String userName = "postgres";
         String password = "1401198n";
         try {
             command.process("connect|Academy|postgres|1401198n");
-        verify(manager).connect(databaseName, userName, password);
-
+            verify(manager).connect(databaseName, userName, password);
+        } catch (ExitException e) {
+            verify(view).write(String.format("The connection to database '%s' for user '%s' is failed due to'%s'", databaseName, userName, e.getMessage()));
         }
-        catch(ExitException e){
-        verify(view).write(String.format("The connection to database '%s' for user '%s' is failed due to'%s'", databaseName, userName, e.getMessage()));
     }
-
-
-
-    }
-
-
-
-  }
+}
