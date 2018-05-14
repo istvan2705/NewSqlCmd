5305c38210e70149905565310ebabfbfab91bb17
Stepan <stepankish@hmail.com>
2018-05-14 15:42:50 +0300

    changes

--- src/main/java/ua/com/juja/sqlcmd/controller/Main.java
+++ src/main/java/ua/com/juja/sqlcmd/controller/Main.java
@@ -2,7 +2,9 @@ package ua.com.juja.sqlcmd.controller;
 
 public class Main {
     public static void main(String[] args) {
-        MainController controller = new MainController();
+
+        MainController controller = MainController.getInstance();
         controller.run();
+
     }
 }
--- src/main/java/ua/com/juja/sqlcmd/controller/MainController.java
+++ src/main/java/ua/com/juja/sqlcmd/controller/MainController.java
@@ -13,14 +13,40 @@ import java.util.List;
 
 public class MainController {
 
-    private View view;
-    private DatabaseManager manager;
+    private static MainController instance;
+    private CommandsManager commandsManager;
+    private List<Command> commandList;
+    private View view =  new Console();
+    private DatabaseManager manager = new JDBCDatabaseManager();
 
-    MainController() {
-        this.view = new Console();
-        this.manager = new JDBCDatabaseManager();
+    private MainController() {
+        loadCommands();
     }
 
+    public static synchronized MainController getInstance() {
+        if (instance == null) {
+            instance = new MainController();
+        }
+        return instance;
+    }
+
+    private void loadCommands() {
+        commandList = new ArrayList<>();
+        commandList.add(new Connect(manager, view));
+        commandList.add(new Exit(view));
+        commandList.add(new Help(view));
+        commandList.add(new isConnected(manager, view));
+        commandList.add(new Create(manager, view));
+        commandList.add(new Clear(manager, view));
+        commandList.add(new Delete(manager, view));
+        commandList.add(new Drop(manager, view));
+        commandList.add(new Find(manager, view));
+        commandList.add(new Insert(manager, view));
+        commandList.add(new Tables(manager, view));
+        commandList.add(new Update(manager, view));
+        commandList.add(new Unsupported(view));
+
+    }
     public void run() {
         view.write("Hello user!");
         view.write("Please enter database, username and password in a format: connect|database|userName|password");
@@ -29,26 +55,11 @@ public class MainController {
             if (input == null) {
                 new Exit(view).process(null);
             }
-            List<Command> commandList = new ArrayList<>();
-            commandList.add(new Connect(manager, view));
-            commandList.add(new Exit(view));
-            commandList.add(new Help(view));
-            commandList.add(new isConnected(manager, view));
-            commandList.add(new Create(manager, view));
-            commandList.add(new Clear(manager, view));
-            commandList.add(new Delete(manager, view));
-            commandList.add(new Drop(manager, view));
-            commandList.add(new Find(manager, view));
-            commandList.add(new Insert(manager, view));
-            commandList.add(new Tables(manager, view));
-            commandList.add(new Update(manager, view));
-            commandList.add(new Unsupported(view));
-            CommandsManager commandsManager = new CommandsManager(commandList);
+              commandsManager = new CommandsManager(commandList);
             commandsManager.result(input);
-
             view.write("Please enter command input(or help):");
+            }
         }
     }
-}
 
 
