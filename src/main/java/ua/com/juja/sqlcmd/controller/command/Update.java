df9517ebb3041d0cfea16400f8c75ddc5e292ff4
Stepan <stepankish@hmail.com>
2018-05-20 20:40:00 +0300

    changes

--- src/main/java/ua/com/juja/sqlcmd/controller/command/Update.java
+++ src/main/java/ua/com/juja/sqlcmd/controller/command/Update.java
@@ -8,14 +8,15 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class Update extends DataClass implements Command {
+
     private DatabaseManager manager;
     private View view;
-    private CommandSeparator commandSeparator = new CommandSeparator(CommandsList.UPDATE);
 
     public Update(DatabaseManager manager, View view) {
         this.manager = manager;
         this.view = view;
     }
+
 
     @Override
     public boolean canProcess(String command) {
@@ -24,16 +25,16 @@ public class Update extends DataClass implements Command {
 
     @Override
     public void process(String command) {
-       List<String> data = getTableData(command);
-        if (data.size() < 6 || data.size() % 2 == 1) {
+        List<String> name = getName(command);
+        List<String> values = getDataTable(command);
+        if (values.size() < 4 || values.size() % 2 == 1) {
             view.write(String.format("Error entering command '%s'. Should be 'update|tableName|column1|value1|" +
                     "column2|value2|...|columnN|valueN", command));
             return;
         }
-        String tableName = getTableName(data);
-        List<String> tableData = commandSeparator.getSeparationResult(command);
-        DataSet set = getDataSet(tableData);
-        String id = data.get(3);
+        String tableName = getTableName(name);
+        DataSet set = getDataSet(values);
+        String id = values.get(1);
         try {
             manager.update(tableName, id, set);
             view.write("The row has been updated");
