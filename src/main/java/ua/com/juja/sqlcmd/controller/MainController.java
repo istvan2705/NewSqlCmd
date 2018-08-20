b76aa13eae8b7fd8f4305dd36aeff4af4f1d5470
Stepan <stepankish@hmail.com>
2018-08-20 14:36:49 +0300

    оптимізовано парсер для команд

--- src/main/java/ua/com/juja/sqlcmd/controller/MainController.java
+++ src/main/java/ua/com/juja/sqlcmd/controller/MainController.java
@@ -4,8 +4,6 @@ import ua.com.juja.sqlcmd.controller.command.Command;
 import ua.com.juja.sqlcmd.controller.command.*;
 import ua.com.juja.sqlcmd.model.DataSet;
 import ua.com.juja.sqlcmd.model.DatabaseManager;
-import ua.com.juja.sqlcmd.model.JDBCDatabaseManager;
-import ua.com.juja.sqlcmd.view.Console;
 import ua.com.juja.sqlcmd.view.View;
 
 
