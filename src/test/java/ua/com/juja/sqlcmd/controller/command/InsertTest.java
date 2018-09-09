a899e229f526fc17742da77f264a1dd7d3ecf67c
istvan <https://github.com/istvan2705/NewSqlCmd.git>
2018-09-09 15:15:42 +0300

    изменения

--- src/test/java/ua/com/juja/sqlcmd/controller/command/InsertTest.java
+++ src/test/java/ua/com/juja/sqlcmd/controller/command/InsertTest.java
@@ -49,7 +49,7 @@ public class InsertTest {
 
         String input = "insert|" + tableName + "|" + column1 + "|" + value1 + "|" + column2 + "|" + value2;
 
-        Map<String, String> map = new LinkedHashMap<>();
+        Map<String, String> map = new HashMap<>();
         map.put(column1, value1);
         map.put(column2, value2);
         when(data.getParameters(input)).thenReturn(new ArrayList(Arrays.asList("insert", "teachers", "id", "3",
@@ -69,14 +69,18 @@ public class InsertTest {
     @Test
     public void testInsertIfRowExists() {
         String tableName = "teachers";
-        Map<String, String> set = new HashMap<>();
-        set.put("id", "3");
-        set.put("surname", "Ivanov");
-        set.put("subject", "History");
-        set.put("city", "Lviv");
+        String column1 = "id";
+        String value1 = "3";
+        String column2 = "surname";
+        String value2 = "Ivanov";
+        String input = "insert|" + tableName + "|" + column1 + "|" + value1 + "|" + column2 + "|" + value2;
+
+        Map<String, String> map = new HashMap<>();
+        map.put(column1, value1);
+        map.put(column2, value2);
         try {
-            command.process("insert|teachers|id|3|surname|Ivanov|subject|History|city|Lviv");
-            verify(manager).insert(tableName, set);
+            command.process(input);
+            verify(manager).insert(tableName, map);
         } catch (SQLException e) {
             verify(view).write(String.format(SQL_EXCEPTION_MESSAGE, e.getMessage()));
         }
