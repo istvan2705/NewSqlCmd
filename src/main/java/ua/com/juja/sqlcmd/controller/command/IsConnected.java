568c7a4f6808c3c170ee8cd921e14f01abf17ed0
istvan <https://github.com/istvan2705/NewSqlCmd.git>
2018-10-14 18:47:07 +0300

    изменения

--- src/main/java/ua/com/juja/sqlcmd/controller/command/IsConnected.java
+++
@@ -1,8 +0,0 @@
-package ua.com.juja.sqlcmd.controller.command;
-public class IsConnected implements Command {
-
-       @Override
-       public String getStatusProcess(){
-        return "You can not use this command until you have established connection to the database";
-    }
-}
