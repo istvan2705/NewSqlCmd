06a867a88f9898cdc860481357f9c8cb9e2fdc3a
istvan <https://github.com/istvan2705/NewSqlCmd.git>
2018-10-26 22:46:22 +0300

    изменения

--- src/main/java/ua/com/juja/sqlcmd/controller/command/Find.java
+++ src/main/java/ua/com/juja/sqlcmd/controller/command/Find.java
@@ -43,7 +43,7 @@ public class Find implements Command {
         }
         return "--------------------------" + "\n" +
                 result.toString() + "\n" +
-                "--------------------------" +"\n";
+               "--------------------------" +"\n";
     }
 
     private String printTableRows(List<String> rows){
